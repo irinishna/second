0ad092d95c1ab3141ebde6219d933eb8be9671f6
irinishna <ira160296@gmail.com>
2021-07-11 15:35:09 +0300

    Lesson_02 fixed, added trafic_light

--- lesson_02.py
+++ lesson_02.py
@@ -1,13 +1,12 @@
 password = "q1w2e3r4"
 x = 3
-while x > 0 :
+while x > 0:
     P = input("Enter password:")
-    x -= 1
-    if P == password :
+    x -= 1  # х = x-1
+    if P == password:
         print("Access granted")
         break
-    elif x == 0 :
+    if x == 0:
         print("You have no attempts left")
-    else :
+    else:
         print("Access denied, attempts left:", x)
-
---
+++ traffic_light.py
@@ -0,0 +1,16 @@
+while True:
+    color = input("Enter color:")
+    if color == "red":
+        print("stop")
+    elif color == "yellow":
+        print("ready")
+    elif color == "green":
+        print("go")
+    else:
+        print("doesn't work")
+        break
+# a = 10
+# print(a)
+# while a > 0:
+#     print(f"{a}")
+#     a -= 1  # a = a - 1
